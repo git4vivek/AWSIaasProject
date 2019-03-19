a968d5346d7607d16e4c0de6d95b86775a678b0b
Leroy Vargis <leroyjvargis@gmail.com>
2019-03-19 00:45:44 -0700

    refactor code to be more OOP

--- autoscaler.py
+++ autoscaler.py
@@ -7,107 +7,129 @@
 # if max < curr, delete.
 # 
 
-import time
+import threading, time
 import boto3
 from collections import deque
 
-inputQueueUrl = 'https://us-west-1.queue.amazonaws.com/079683809430/scaler-test-q-1' #the input queue
-dontDeleteQueueUrl = '' 
-amiId = 'ami-0e355297545de2f82' #update with our AMI
-timeSlotDuration = 10 #seconds
-region = 'us-west-1'
+class AutoScaler:
 
-slots = deque([0, 0, 0, 0, 0])
+    def __init__(self, inputQueueUrl, dontDeleteQueueUrl, amiId, timeSlotDuration = 60, region = 'us-west-1'):
+        self.inputQueueUrl = inputQueueUrl # 'https://us-west-1.queue.amazonaws.com/079683809430/scaler-test-q-1' #the input queue
+        self.dontDeleteQueueUrl = dontDeleteQueueUrl
+        self.amiId = amiId #'ami-0e355297545de2f82' #update with our AMI
+        self.timeSlotDuration = timeSlotDuration #seconds
+        self.region = region
+        self.slots = deque([0, 0, 0, 0, 0])
+        
+        self.sqs = boto3.client('sqs', region_name=region)
+        self.ec2 = boto3.resource('ec2', region_name=region)
+        self.currentInstances = None
 
-def getCountOfMessagesInSQS():
-    sqs = boto3.client('sqs', region_name=region)
+        ## start as bg thread
+        # thread = threading.Thread(target=self.run, args=())
+        # thread.daemon = True
+        # thread.start()
 
-    response = sqs.get_queue_attributes(
-        QueueUrl=inputQueueUrl,
-        AttributeNames=['ApproximateNumberOfMessages']
-    )
+        ## debug:
+        self.run()
 
-    return int(response['Attributes']['ApproximateNumberOfMessages'])
+    def getCountOfMessagesInSQS(self):
+        response = self.sqs.get_queue_attributes(
+            QueueUrl=self.inputQueueUrl,
+            AttributeNames=['ApproximateNumberOfMessages']
+        )
 
-def getInstances():
-    # get currently running instances
-    ec2 = boto3.resource('ec2', region_name=region)
-    instances = ec2.instances.filter(
-        Filters=[
-            {
-                'Name': 'image-id',
-                'Values': [
-                    amiId,
-                ]
-            },
-            {
-                'Name': 'instance-state-name',
-                'Values': [
-                    'running',
-                ]
-            }
-        ])
+        return int(response['Attributes']['ApproximateNumberOfMessages'])
+
+
+    def getInstances(self, states=['running']):
+        ## get currently running instances
+        instances = self.ec2.instances.filter(
+            Filters=[
+                {
+                    'Name': 'image-id',
+                    'Values': [
+                        self.amiId,
+                    ]
+                },
+                {
+                    'Name': 'instance-state-name',
+                    'Values': states
+                }
+            ])
+        
+        instanceList = []
+        for instance in instances:
+            instanceList.append(instance.id)
+
+        return instanceList
+
+
+    def createInstances(self, count = 1):
+        ## may need to set up VPC and SG
+        currentlyCreatingInstances = len(self.getInstances(states=['pending']))
+        if currentlyCreatingInstances != count:
+            instance = self.ec2.create_instances(
+                ImageId = self.amiId,
+                InstanceType = 't2.micro',
+                MaxCount = count - currentlyCreatingInstances,
+                MinCount = 1
+            )
+            print ('creating ' + str(count) + ' new instances')
+        else:
+            print (str(count) + ' instances already started init')
     
-    instanceList = []
-    for instance in instances:
-        instanceList.append(instance.id)
-
-    return instanceList
-
-def createInstances(count = 1):
-    ## may need to set up VPC and SG
-    ec2 = boto3.resource('ec2', region_name='us-west-1')
-    instance = ec2.create_instances(
-        ImageId=amiId,
-        InstanceType='t2.micro',
-        MaxCount=count,
-        MinCount=1
-    )
-    print ('creating ' + str(count) + ' instances')
- 
-
-def getInstancesToDelete(currentInstances = None):
-    #talk to DontDeleteQ
-    #return instances not in DontDeleteQ
-
-    if currentInstances == None:
-        currentInstances = getInstances()
-    
-    return currentInstances
 
 
-def deleteInstances(currentInstances, count):
-    instanceList = getInstancesToDelete(currentInstances)
-    instanceList = instanceList[:count]
-    print ("instances to delete: ", instanceList)
-    ec2 = boto3.resource('ec2', region_name=region)
-    ec2.instances.filter(InstanceIds = instanceList).terminate()
+    def getInstancesToDelete(self):
+        ## talk to DontDeleteQ
+        ## return instances not in DontDeleteQ
 
-while True:
-    currentInstances = getInstances() # list of instance Ids currently running
-    currentInstanceCount = len(currentInstances)
-    currentMessageCount = getCountOfMessagesInSQS()
+        ## debug code until implemented
+        if self.currentInstances == None:
+            self.currentInstances = self.getInstances()
+        
+        return self.currentInstances
 
-    slots.append(currentMessageCount)
-    slots.popleft()
-    mvMaxOfMessages = max(slots)
 
-    print ("slots", slots)
-    print ("mvMaxOfMessages", mvMaxOfMessages)
-    print ("currentInstanceCount", currentInstanceCount)
-    if mvMaxOfMessages > currentInstanceCount and currentInstanceCount < 20:
-        #initCreate
-        instancesToCreate = mvMaxOfMessages - currentInstanceCount if mvMaxOfMessages - currentInstanceCount < 20 else 20
-        createInstances(instancesToCreate)
-        print ("initiating creation of " + str(instancesToCreate) + " instances")
-    elif mvMaxOfMessages < currentInstanceCount:
-        #initDelete
-        instancesToDelete = currentInstanceCount - mvMaxOfMessages
-        print ("initiating delete of " + str(instancesToDelete) + " instances")
-        deleteInstances(currentInstances, instancesToDelete)
-    else:
-        print ("do nothing")
+    def deleteInstances(self, count):
+        instanceList = self.getInstancesToDelete()
+        instanceList = instanceList[:count]
+        print ("instances to delete: ", instanceList)
+        #ec2 = boto3.resource('ec2', region_name=region)
+        self.ec2.instances.filter(InstanceIds = instanceList).terminate()
 
-    print ('Waiting for ' + str(timeSlotDuration) + ' seconds')
-    print ('')
-    time.sleep(timeSlotDuration)
+
+    def run(self):
+        while True:
+            self.currentInstances = self.getInstances() # list of instance Ids currently running
+            currentInstanceCount = len(self.currentInstances)
+            currentMessageCount = self.getCountOfMessagesInSQS()
+
+            self.slots.append(currentMessageCount)
+            self.slots.popleft()
+            mvMaxOfMessages = max(self.slots)
+
+            print ("slots", self.slots)
+            print ("mvMaxOfMessages", mvMaxOfMessages)
+            print ("currentInstanceCount", currentInstanceCount)
+            if mvMaxOfMessages > currentInstanceCount and currentInstanceCount < 20:
+                ## initCreate
+                instancesToCreate = mvMaxOfMessages - currentInstanceCount if mvMaxOfMessages - currentInstanceCount < 20 else 20
+                print ("initiating creation of " + str(instancesToCreate) + " instances")
+                self.createInstances(instancesToCreate)
+            elif mvMaxOfMessages < currentInstanceCount:
+                ## initDelete
+                instancesToDelete = currentInstanceCount - mvMaxOfMessages
+                print ("initiating delete of " + str(instancesToDelete) + " instances")
+                self.deleteInstances(instancesToDelete)
+            else:
+                ## do nothing
+                print ("do nothing")
+
+            print ('Waiting for ' + str(self.timeSlotDuration) + ' seconds')
+            print ('')
+            time.sleep(self.timeSlotDuration)
+
+
+a = AutoScaler(inputQueueUrl = 'https://us-west-1.queue.amazonaws.com/079683809430/scaler-test-q-1', dontDeleteQueueUrl = '', amiId = 'ami-0e355297545de2f82', timeSlotDuration=10)
