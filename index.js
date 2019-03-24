77a5e3da21cb20ecccc801884b9889db00639e28
anique <aniquetahir@gmail.com>
2019-03-23 18:30:03 -0700

    Using external ffmpeg conversion'

--- index.js
+++ index.js
@@ -1,51 +1,28 @@
 const http = require('http');
 const express =  require('express');
-const _ = require('lodash');
-
-const VideoHandler = require('./VideoHandler');
+const AWS = require('aws-sdk');
 
 
 const app = express();
-const port = 3000;
+const port = 80;
 
-const rpi_cluster_ip = '206.207.50.7';
-const video_url = `http://${rpi_cluster_ip}/getvideo/`;
-
+const lambda = new AWS.Lambda({region: 'us-west-2'});
+const lambda_params = {
+    FunctionName: "RekVideo"
+};
 
 app.get('/', (req, res)=>{
-    console.log("Getting video from the RPi cluster...");
-    http.get(video_url, (vid_res)=>{
-
-        // Try to get filename from the video
-        let content_disposition = vid_res.headers["content-disposition"];
-        let content_disposition_kv = content_disposition.split(';').map((kv=>kv.trim().split('=')));
-
-        let filename_kv = content_disposition_kv.filter((kv)=>{
-            return kv[0].trim() === 'filename';
-        })[0];
-
-        let filename = filename_kv[1].split("\"").join("");
-
-        let vh = new VideoHandler(filename, vid_res);
-
-        // Send Video to app server for processing
-        vh.processVideo((results)=>{
-            // Send Results to client
-            res.send(results);
-
-            // Save Results on S3
-            vh.uploadResults(results);
-
-        });
-
-        //res.send('boop');
+    lambda.invoke(lambda_params, (err,data)=>{
+        if(err){
+            console.log(err);
+            res.send(err.toString());
+        }else{
+            console.log(data);
+            res.send(JSON.parse(data['Payload'])['body']);
+        }
     });
-
-    //res.send("Hello");
 });
 
 app.listen(port, ()=>{
-    VideoHandler.printHello();
-
     console.log(`Started server at port ${port}`);
 });
